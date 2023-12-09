89577d494ecb649a165de89df44200eb7fb1cc36
Derek Moy <derekymoy@gmail.com>
2023-12-09 15:23:39 -0800

    Added functionality to show icon representation of weather conditions.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,20 @@
 var APIKey = "6f3316f2ba5c5f4e5a81ee758ba68d1a";
 var queriedCity = $("#city-search");
+
 var cityName = $("#city-name");
-var date = $("#date");
-var currentForecastIcon = $("#weather-icon");
 var currentDate = new Date();
+var date = $("#date");
+var forecastIcon = $("#weather-icon");
 
 var currentTemp = $("#temp");
 var currentWind = $("#wind");
 var currentHumidity = $("#humidity");
+
+// This function takes a temperature value in Kelvin and converts it to Fahrenheit
+function kelvinToFahrenheit(value){
+    var result = ((value-273.15)*1.8)+32;
+    return result.toFixed(2);
+}
 
 function getApi(queryURL) {
     fetch(queryURL)
@@ -22,18 +29,43 @@ function getApi(queryURL) {
     })
     .then (function (data) {
         console.log(data);
-        cityName.text(data.name + ', ' + data.sys.country);
-        date.text("(" + currentDate.toLocaleDateString() + ")");
 
+        // Display the city, date, and icon depicting the current weather condition
+        cityName.text(data.name);
+        date.text("(" + currentDate.toLocaleDateString() + ")");
+        forecastIcon.addClass(getWeatherClass(data.weather[0].id));
+
+        // Display the current temperature, wind speed, and humidity
         currentTemp.text("Temp: " + kelvinToFahrenheit(data.main.temp) + '\u00B0' + 'F');
         currentWind.text("Wind: " + data.wind.speed + " MPH");
         currentHumidity.text("Humidity: " + data.main.humidity + "%");
     })
 }
 
-function kelvinToFahrenheit(value){
-    var result = ((value-273.15)*1.8)+32;
-    return result.toFixed(2);
+// This function returns the class name to add to <i> to show the current weather condition
+function getWeatherClass(code) {
+    var result;
+    if (code > 800) {
+        // cloudy
+        result = "fa-cloud";
+    } else if (code === 800) {
+        // clear 
+        result = "fa-sun";
+    } else if (code > 700 && code < 800) {
+        // hazy or smog
+        result = "fa-smog";
+    } else if (code > 600 && code < 700) {
+        // snow
+        result = "fa-snowflake";
+    } else if (code > 300 && code < 600) {
+        // drizzle or rain
+        result = "fa-cloud-rain";
+    } else if (code < 300) {
+        // thunder
+        result = 'fa-cloud-bolt'
+    }
+
+    return result;s
 }
 
 function fetchCity() {
